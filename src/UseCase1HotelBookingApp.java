9cac832913570c739289249b8add4432dba8cb3c
sarthaksingh218 <ss5652@srmist.edu.in>
2026-03-13 14:18:22 +0530

    UC1: Added welcome message module

--- src/UseCase1HotelBookingApp.java
+++ src/UseCase1HotelBookingApp.java
@@ -1,10 +1,10 @@
-public class BookMyStayApp {
+public class UseCase1HotelBookingApp {
 
     public static void main(String[] args) {
 
         System.out.println("====================================");
-        System.out.println("     Welcome to Hotel Booking Management System App");
-        System.out.println("     Hotel Booking System v1.0");
+        System.out.println("Welcome to My Hotel Booking Management System App");
+        System.out.println("Hotel Booking System v1.0");
         System.out.println("====================================");
 
         System.out.println("System initialized successfully.");
